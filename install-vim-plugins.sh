8087d569929a6cf655aa588c6d203d8aa3307dcc
Lars Bekhof <larsbekhof@gmail.com>
2018-02-27 11:39:25 +0100

    ADDED vim plugin install file

---
+++ install-vim-plugins.sh
@@ -0,0 +1,24 @@
+# Pathogen install
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# ctrlp install
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+
+# delimitMate install
+git clone https://github.com/Raimondi/delimitMate.git ~/.vim/bundle/delimitMate
+
+# nerdtree install
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+# powerline install
+git clone https://github.com/powerline/powerline.git ~/.vim/bundle/powerline
+
+# syntastic install
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
+
+# typescript-vim install
+git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/bundle/typescript-vim
+
+# vim-js-pretty-template install
+git clone https://github.com/Quramy/vim-js-pretty-template.git ~/.vim/bundle/vim-js-pretty-template
